31e2988b83311a6d5ec0560cf0259d100fbbf2f1
PatrickHa <diepvien004b@gmail.com>
2021-08-27 13:04:46 +0700

    phat

---
+++ my-books-data/Configurations/AuthorConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using my_books_data.Entities;
+using System;
+
+namespace my_books_data.Configurations
+{
+    public class AuthorConfiguration : IEntityTypeConfiguration<Author>
+    {
+        public void Configure(EntityTypeBuilder<Author> builder)
+        {
+            builder.ToTable("Authors");
+
+            builder.HasKey(a => a.Id);
+            builder.Property(a => a.Id).UseIdentityColumn().IsRequired();
+
+            builder.Property(a => a.FirstName).HasColumnType("nvarchar(100)").IsRequired();
+            builder.Property(a => a.LastName).HasColumnType("nvarchar(100)").IsRequired();
+        }
+    }
+}
---
+++ my-books-data/Entities/Publisher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_books_data.Entities
+{
+    public class Publisher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Book> Books { get; set; }
+    }
+}
